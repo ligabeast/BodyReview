56f9b427415066af9a9f46094b67d13ba121c651
Baran <Baran_ozbey02@outlook.de>
2023-09-29 22:19:47 +0200

    quick fix

--- App.tsx
+++ App.tsx
@@ -1,18 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
-import { NativeWindStyleSheet } from "nativewind";
-
-NativeWindStyleSheet.setOutput({
-  default: "native",
-});
-
 import Test from "./src/components/Test";
 
 export default function App() {
   return (
     <View className="bg-black">
       <Text className="text-blue-600">
-        Open up App.js to start working on your app!!
+        Open up App.js to start working on your app!!!
       </Text>
       <Test />
       <StatusBar style="auto" />
