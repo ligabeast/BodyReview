f10b39015379125d7a2c398283b7bab098248b0a
Baran <Baran_ozbey02@outlook.de>
2023-10-01 17:20:33 +0200

    init

--- App.tsx
+++ App.tsx
@@ -1,15 +1,8 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Text, TextInput, View, Button } from "react-native";
+import { useState } from "react";
 import Test from "./src/components/Test";
 
 export default function App() {
-  return (
-    <View className="bg-black">
-      <Text className="text-blue-600">
-        Open up App.js to start working on your app!!!
-      </Text>
-      <Test />
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [TextInput, onChangeTextInput] = useState("");
+  return <Test></Test>;
 }
